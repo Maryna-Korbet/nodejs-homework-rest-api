cffb1f187efd012a1bd3f41f12c78fce02c46aa6
Maryna-Korbet <m22v86k@gmail.com>
2023-03-22 23:42:31 +0200

    added avatar

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const ctrl = require("../../controllers/auth")
 
-const {validateBody, authenticate, validateSubscription} = require("../../middlewares")
+const {validateBody, authenticate, validateSubscription, upload} = require("../../middlewares")
 
 const {schemas} = require("../../models/user")
 
@@ -17,5 +17,7 @@ router.get("/current", authenticate, ctrl.getCurrent)
 router.post("/logout", authenticate, ctrl.logout)
 
 router.patch("/", authenticate, validateSubscription, validateBody(schemas.subscriptionSchema), ctrl.updateStatusSubscription)
+
+router.patch("/avatars", authenticate, upload.single("avatar"), ctrl.updateAvatar)
 
 module.exports = router
